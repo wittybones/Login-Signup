844fd8bda194f352f4c9d16776813616aa2f8f8e
Tushar Tambe <tmtushar@thoughtworks.com>
2019-08-24 12:45:29 +0530

    Simple Login Page created

--- src/HomePage.jsx
+++ src/HomePage.jsx
@@ -1,38 +1,62 @@
 import React, { Component } from 'react';
 import './App.css';
-import { Tab, Tabs } from "react-bootstrap";
+import { Tab, Tabs, Button, Form } from "react-bootstrap";
 
 class HomePage extends Component {
   constructor(props) {
     super(props)
     this.state = {
       activeItem: "login"
-    };
+    }; 
   }
 
   toggle(key) {
     this.setState({ activeItem: key })
   };
 
+
   render() {
-    return (<div className="login-signup-div" >
-      <Tabs activeKey={this.state.activeItem} defaultActiveKey="login" id="tabs" onSelect={(key) => this.toggle(key)}>
-        <Tab eventKey="login" className='login-container' title="Login">
-          <form className="loginForm">
-            <label htmlFor="">Email Id : </label> <input type="Email" />
-            <label htmlFor="">Password : </label> <input type="Password" />
-            <input type="submit" className='submitBtn' />
-            <a onClick={() => this.toggle('signup')} className="signupToggle">Dont Have an account? signup</a>
-          </form>
+    return (<div className="formBox">
+      <Tabs activeKey={this.state.activeItem} defaultActiveKey="login" onSelect={(key) => this.toggle(key)}>
+        <Tab eventKey="login" title="Login">
+        <Form>
+            <Form.Group controlId="formBasicUserName">
+              <Form.Label >Username</Form.Label>
+              <Form.Control size="lg" placeholder="yourusername" />
+            </Form.Group>
+            <Form.Group controlId="formBasicPassword">
+              <Form.Label>Password</Form.Label>
+              <Form.Control type="password" size="lg" placeholder="Password" />
+            </Form.Group>
+            <br/>
+            <Button variant="primary" type="submit" size="lg" block>
+              Submit
+            </Button>
+          </Form>
         </Tab>
-        <Tab eventKey="signup" className="signup-container" title="SignUp">
-          <form className="signupForm">
-            <label htmlFor="">Username : </label> <input type="text" />
-            <label htmlFor="">Email Id : </label> <input type="Email" />
-            <label htmlFor="">Password : </label> <input type="Password" />
-            <label htmlFor="">confirm Password : </label> <input type="Password" />
-            <input className='submitBtn' type="submit" onClick={() => this.toggle('login')} />
-          </form>
+        <Tab eventKey="signUp" title="Sign Up">
+          <Form>
+            <Form.Group controlId="formBasicEmail">
+              <Form.Label>Email address</Form.Label>
+              <Form.Control type="email" size="lg" placeholder="Enter email" />
+            </Form.Group>
+            <Form.Group controlId="formBasicUserName">
+              <Form.Label>Username</Form.Label>
+              <Form.Control size="lg" placeholder="yourusername" />
+            </Form.Group>
+            <Form.Group controlId="formBasicPassword">
+              <Form.Label>Password</Form.Label>
+              <Form.Control type="password" size="lg" placeholder="Password" />
+            </Form.Group>
+            <Form.Group controlId="formBasicConfirmPassword">
+              <Form.Label>Confirm Password</Form.Label>
+              <Form.Control type="password"size="lg" placeholder="Confirm Password" />
+            </Form.Group>
+            <br/>
+            <Button variant="primary" type="submit" size="lg" block>
+              Submit
+            </Button>
+          </Form>
         </Tab>
       </Tabs>
     </div>);
